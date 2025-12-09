87eee14d0e70fc1d8d07eddb4ea95d9d09ffdd56
Salamdi <dinislam.abdulsalamov@gmail.com>
2025-12-09 08:55:52 +0100

    update nav name

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import Link from "next/link";
-import { useSelectedLayoutSegment } from "next/navigation";
-
 import { cn } from "@/lib/utils";
-import { Icons } from "@/components/icons";
-import { Button, buttonVariants } from "./ui/button";
+import { Button } from "./ui/button";
 import { EditorMenubar } from "./editor-menubar";
-import { Download, Loader2Icon, Settings } from "lucide-react";
+import { Download, Loader2Icon } from "lucide-react";
 import Pager from "./pager";
 import { FilenameForm } from "./forms/filename-form";
 import Logo from '../app/icon1.png';
@@ -37,7 +34,7 @@ export function MainNav({ handlePrint, isPrinting, className }: MainNavProps) {
         <Link href="/" className="items-center space-x-2 flex">
           <img src={Logo.src} className="w-10 h-10" />
           <span className="hidden font-bold md:inline-block">
-            Carousel Generator
+            Generate Carousel
           </span>
         </Link>
         <EditorMenubar />
